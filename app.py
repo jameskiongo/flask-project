d37ad75830093622e21bda6e23e7752dc29a8a17
jameskiongo <kiongojames2000@gmail.com>
2020-04-01 18:10:05 +0300

    updated

--- app.py
+++ app.py
@@ -5,10 +5,19 @@ app = Flask(__name__)
 def hello():
 	return "hello world"
 
+@app.route("/login")
+def login():
+	return "login page"
+
+@app.route("/signup")
+def signup():
+	return "signup"
+
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return "ERROR: {} ".format(e),404
+    return "ERROR: {} ".format(e),404 
+     
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
